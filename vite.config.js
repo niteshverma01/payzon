d3df742500a86fea8f0de0c28212d0736983bab9
Nitesh Verma <niteshverma9025@gmail.com>
2025-06-26 17:19:18 +0530

    add some new

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   server: {
@@ -8,9 +12,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-         main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'index.html'),
         product: resolve(__dirname, 'product.html'),
       }
     }
   }
-}); 
+});
